c41b2d70adee381446da90eb2304a49bb059a7e7
brandonnegen <bnegen@gmail.com>
2015-08-01 18:16:33 -0500

    Basic Functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,42 +5,46 @@ $(document).ready(function(){
             console.log(data);
             console.log(data.person1.name);
             $.each(data, function(){
-                $('.name').text(this.name);//sends JSON.name to the '.name' class in the html
-                $('.description').text(this.desc);//sends JSON.desc to the '.description' class in the html
-                $('.shout-out').text(this.thanks);//sendsJSON.thanks to the '.shout-out' class in the html
+                $('.whole-cohort').append("<div class='each-student'></div>");
+                var eachStudent = $('.whole-cohort').children().last();
+                eachStudent.append("<h1 class='name'>" + this.name + "</h1>");
+                eachStudent.append("<p class='description'>" + this.desc + "</p>");
+                eachStudent.append("<p class='shout-out'>" + this.thanks + "</p>");
                 console.log(this.name);
-                $('.arrow-next').click(function(){
-                    var currentSlide = $('.active-slide');
-                    var nextSlide = currentSlide.next();
-                    if(nextSlide.length == 0){
-                        nextSlide = $('.slide').first();
-                    }
-                    currentSlide.fadeOut(600).removeClass('active-slide');
-                    nextSlide.fadeIn(600).addClass('active-slide');
-                    var currentDot = $('.active-dot');
-                    var nextDot = currentDot.next();
-                    if(nextDot.length == 0){
-                        nextDot = $('.dot').first();
-                    }
-                    currentDot.removeClass('active-dot');
-                    nextDot.addClass('active-dot');
-                });
-                $('.arrow-prev').click(function(){
-                    var currentSlide = $('.active-slide');
-                    var prevSlide = currentSlide.prev();
-                    if(prevSlide.length == 0){
-                        prevSlide = $('.slide').last();
-                    }
-                    currentSlide.fadeOut(600).removeClass('active-slide');
-                    prevSlide.fadeIn(600).addClass('active-slide');
-                    var currentDot = $('.active-dot');
-                    var prevDot = currentDot.prev();
-                    if(prevDot.length == 0){
-                        prevDot = $('.dot').last();
-                    }
-                    currentDot.removeClass('active-dot');
-                    prevDot.addClass('active-dot');
-                });
+
+            });
+            $('.each-student:first').addClass('show-student');
+            $('.arrow-next').click(function(){
+                var currentSlide = $('.show-student');
+                var nextSlide = currentSlide.next();
+                if(nextSlide.length == 0){
+                    nextSlide = $('.each-student').first();
+                }
+                currentSlide.fadeOut(600).removeClass('show-student');
+                nextSlide.fadeIn(600).addClass('show-student');
+                var currentDot = $('.active-dot');
+                var nextDot = currentDot.next();
+                if(nextDot.length == 0){
+                    nextDot = $('.dot').first();
+                }
+                currentDot.removeClass('active-dot');
+                nextDot.addClass('active-dot');
+            });
+            $('.arrow-prev').click(function(){
+                var currentSlide = $('.show-student');
+                var prevSlide = currentSlide.prev();
+                if(prevSlide.length == 0){
+                    prevSlide = $('.each-student').last();
+                }
+                currentSlide.fadeOut(600).removeClass('show-student');
+                prevSlide.fadeIn(600).addClass('show-student');
+                var currentDot = $('.active-dot');
+                var prevDot = currentDot.prev();
+                if(prevDot.length == 0){
+                    prevDot = $('.dot').last();
+                }
+                currentDot.removeClass('active-dot');
+                prevDot.addClass('active-dot');
             });
         }
     });
